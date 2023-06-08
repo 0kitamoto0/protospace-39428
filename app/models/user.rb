cf18e5df83ffd8ce04aa8603a9f3902667cb1e8f
ttk990 <tatuki990715@gmail.com>
2023-06-08 16:54:15 +0900

    user修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,9 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true
   validates :password, length: { minimum: 6 }
+  validates :profile, presence: true
+  validates :occupation, presence: true
+  validates :position, presence: true
   has_many :prototypes
   has_many :comments
 end
